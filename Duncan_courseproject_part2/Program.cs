1a0d84f0bb22e8288fc2a65240f04fbea801fedf
JeremyDuncan <jduncan46@my.devry.edu>
2022-05-30 07:59:47 -0400

    Added listbox, and 4 buttons to form

--- Duncan_courseproject_part2/Program.cs
+++ Duncan_courseproject_part2/Program.cs
@@ -16,7 +16,7 @@ namespace Duncan_courseproject_part2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
