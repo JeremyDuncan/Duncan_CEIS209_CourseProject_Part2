4fa286ecae603ddf587d53a3c145454ca5343d56
JeremyDuncan <jduncan46@my.devry.edu>
2022-05-30 10:28:58 -0400

    Added employee class

---
+++ Duncan_courseproject_part2/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duncan_courseproject_part2
+{
+    internal class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private string ssn;
+        private DateTime hireDate;
+
+        // Constructors
+
+        // Behaviors
+
+        // Properties
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    firstName = "unknown";
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    lastName = "unknown";
+                }
+            }
+        }
+
+    }
+}
