a705a401af1befcacff67e02c0659b3f07cf9fba
JeremyDuncan <jduncan46@my.devry.edu>
2022-06-06 09:26:38 -0400

    Created Submit and Cancel buttons

--- Duncan_courseproject_part2/inputForm.Designer.cs
+++ Duncan_courseproject_part2/inputForm.Designer.cs
@@ -29,13 +29,35 @@
         private void InitializeComponent()
         {
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(46, 194);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 0;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(140, 194);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
             // 
             // inputForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 333);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.SubmitButton);
             this.Name = "inputForm";
             this.Text = "inputForm";
             this.ResumeLayout(false);
@@ -45,5 +67,7 @@
         #endregion
 
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Button CancelButton;
     }
 }
