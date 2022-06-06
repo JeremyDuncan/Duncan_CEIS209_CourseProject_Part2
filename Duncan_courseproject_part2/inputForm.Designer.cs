1788bc06893a1b0cceeab27e0a112d1e9d506ed4
JeremyDuncan <jduncan46@my.devry.edu>
2022-06-06 09:34:11 -0400

    Created textboxes and labels

--- Duncan_courseproject_part2/inputForm.Designer.cs
+++ Duncan_courseproject_part2/inputForm.Designer.cs
@@ -31,11 +31,19 @@
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SubmitButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.SSNTextBox = new System.Windows.Forms.TextBox();
+            this.HireDateTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SubmitButton
             // 
-            this.SubmitButton.Location = new System.Drawing.Point(46, 194);
+            this.SubmitButton.Location = new System.Drawing.Point(25, 162);
             this.SubmitButton.Name = "SubmitButton";
             this.SubmitButton.Size = new System.Drawing.Size(75, 23);
             this.SubmitButton.TabIndex = 0;
@@ -44,23 +52,96 @@
             // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(140, 194);
+            this.CancelButton.Location = new System.Drawing.Point(115, 162);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(75, 23);
             this.CancelButton.TabIndex = 1;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = true;
             // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(80, 24);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(120, 20);
+            this.FirstNameTextBox.TabIndex = 2;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(80, 59);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(120, 20);
+            this.LastNameTextBox.TabIndex = 3;
+            // 
+            // SSNTextBox
+            // 
+            this.SSNTextBox.Location = new System.Drawing.Point(80, 95);
+            this.SSNTextBox.Name = "SSNTextBox";
+            this.SSNTextBox.Size = new System.Drawing.Size(120, 20);
+            this.SSNTextBox.TabIndex = 4;
+            // 
+            // HireDateTextBox
+            // 
+            this.HireDateTextBox.Location = new System.Drawing.Point(80, 131);
+            this.HireDateTextBox.Name = "HireDateTextBox";
+            this.HireDateTextBox.Size = new System.Drawing.Size(120, 20);
+            this.HireDateTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "SSN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Hire Date:";
+            // 
             // inputForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 333);
+            this.ClientSize = new System.Drawing.Size(219, 197);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HireDateTextBox);
+            this.Controls.Add(this.SSNTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
             this.Controls.Add(this.CancelButton);
             this.Controls.Add(this.SubmitButton);
             this.Name = "inputForm";
             this.Text = "inputForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +150,13 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button SubmitButton;
         private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox SSNTextBox;
+        private System.Windows.Forms.TextBox HireDateTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
